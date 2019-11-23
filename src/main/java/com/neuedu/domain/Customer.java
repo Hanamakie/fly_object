a15c010ba60c580c602b48488f808c4a207a4440
changniupi <1157414727@qq.com>
2019-11-23 22:22:48 +0800

    Signed-off-by: changniupi <1157414727@qq.com>

---
+++ src/main/java/com/neuedu/domain/Customer.java
@@ -0,0 +1,61 @@
+package com.neuedu.domain;
+
+public class Customer {
+	private int id;
+	String username;
+	String password;
+	String nick_name;
+	String email;
+	int status;
+	int vip_level;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getNick_name() {
+		return nick_name;
+	}
+	public void setNick_name(String nick_name) {
+		this.nick_name = nick_name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public int getVip_level() {
+		return vip_level;
+	}
+	public void setVip_level(int vip_level) {
+		this.vip_level = vip_level;
+	}
+	
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", username=" + username + ", password=" + password + ", nick_name=" + nick_name
+				+ ", email=" + email + ", status=" + status + ", vip_level=" + vip_level + "]";
+	}
+	
+}
