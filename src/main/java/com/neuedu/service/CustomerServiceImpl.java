390f29e74f88cc046ff53b60f1c6516b0e88f93c
unknown <you@example.com>
2019-11-27 16:53:29 +0800

    Signed-off-by: unknown <you@example.com>

--- src/main/java/com/neuedu/service/CustomerServiceImpl.java
+++ src/main/java/com/neuedu/service/CustomerServiceImpl.java
@@ -9,7 +9,7 @@ import com.neuedu.domain.Customer;
 @Service
 public  class CustomerServiceImpl implements CustomerService{
 	@Autowired
-	CustomerMapper customerMapper;
+	private CustomerMapper customerMapper;
 	//登陆，通过账号和密码或者邮箱和密码查询用户
 	@Override
 	public Customer getCustomer(Customer customer) {
@@ -20,6 +20,40 @@ public  class CustomerServiceImpl implements CustomerService{
 		return customer2;
 	}
 
+	/*添加邮箱*/
+	@Override
+	public void addcustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		customerMapper.addcustomer(customer);
+	}
+
+	/*邮箱验证*/
+	@Override
+	public String testemail(String email) {
+		// TODO Auto-generated method stub
+		Customer customer = customerMapper.testemail(email);
+		String s = "";
+		if(customer == null) {
+			s = "OK";
+		}else {
+			s = "NO";
+		}
+		return s;
+	}
+
+	/*用户名验证*/
+	@Override
+	public String testusername(String username) {
+		// TODO Auto-generated method stub
+		Customer customer = customerMapper.testusername(username);
+		String s1 = "";
+		if(customer == null) {
+			s1 = "OK";
+		}else {
+			s1 = "NO";
+		}
+		return s1;
+	}
 	
 
 	
