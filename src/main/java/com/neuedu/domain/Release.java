bd880e88b782b598e049f8e1b38c49949bc03b3d
Hanamakie <13844804651@163.com>
2019-12-09 21:56:52 +0800

    Signed-off-by: Hanamakie <13844804651@163.com> '查询文章信息问题--mapper有问题----抛'

--- src/main/java/com/neuedu/domain/Release.java
+++ src/main/java/com/neuedu/domain/Release.java
@@ -1,48 +1,22 @@
 package com.neuedu.domain;
 
 import java.util.Date;
-import java.util.List;
 
 public class Release {
 	private int id; // 文章标识
 	private String title; // 文章标题
 	private String content; // 文章内容
 	private int user_id; // 发表人
-	private List<Customer> customer;//用户信息
-	private List<Kiss> kiss;//飞吻数
-	private List<Level> level;//会员等级
-	private List<Special> special;//文章分类
+	private Customer customer;//用户信息
+	private Kiss kiss;//飞吻数
+	private Level level;//会员等级
+	private Special special;//文章分类
 	private Date create_time; // 创建时间
 	private int view_count; // 访问次数
 	private int special_column_id; // 文章分类
 	private int fly_kiss_id; // 飞吻数
 	private int sort_order; // 排序
 	private int sticky; // 指定级别
-	
-	public List<Level> getLevel() {
-		return level;
-	}
-	public void setLevel(List<Level> level) {
-		this.level = level;
-	}
-	public List<Special> getSpecial() {
-		return special;
-	}
-	public void setSpecial(List<Special> special) {
-		this.special = special;
-	}
-	public List<Customer> getCustomer() {
-		return customer;
-	}
-	public void setCustomer(List<Customer> customer) {
-		this.customer = customer;
-	}
-	public List<Kiss> getKiss() {
-		return kiss;
-	}
-	public void setKiss(List<Kiss> kiss) {
-		this.kiss = kiss;
-	}
 	public int getId() {
 		return id;
 	}
@@ -66,6 +40,30 @@ public class Release {
 	}
 	public void setUser_id(int user_id) {
 		this.user_id = user_id;
+	}
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	public Kiss getKiss() {
+		return kiss;
+	}
+	public void setKiss(Kiss kiss) {
+		this.kiss = kiss;
+	}
+	public Level getLevel() {
+		return level;
+	}
+	public void setLevel(Level level) {
+		this.level = level;
+	}
+	public Special getSpecial() {
+		return special;
+	}
+	public void setSpecial(Special special) {
+		this.special = special;
 	}
 	public Date getCreate_time() {
 		return create_time;
@@ -106,8 +104,10 @@ public class Release {
 	@Override
 	public String toString() {
 		return "Release [id=" + id + ", title=" + title + ", content=" + content + ", user_id=" + user_id
+				+ ", customer=" + customer + ", kiss=" + kiss + ", level=" + level + ", special=" + special
 				+ ", create_time=" + create_time + ", view_count=" + view_count + ", special_column_id="
 				+ special_column_id + ", fly_kiss_id=" + fly_kiss_id + ", sort_order=" + sort_order + ", sticky="
 				+ sticky + "]";
 	}
+
 }
